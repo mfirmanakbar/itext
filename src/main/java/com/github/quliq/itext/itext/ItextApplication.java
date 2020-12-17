2098118f2d8c735a9ef1a6ecf29ae44fb8d7052b
mfirmanakbar <em.firman.akbar@gmail.com>
2020-12-17 10:29:40 +0700

    random char with lengh Long or 9223372036854775807

--- src/main/java/com/github/quliq/itext/itext/ItextApplication.java
+++ src/main/java/com/github/quliq/itext/itext/ItextApplication.java
@@ -3,42 +3,48 @@ package com.github.quliq.itext.itext;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class ItextApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItextApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ItextApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
+        print("\n");
+        print("Enter number of length: ");
+        Scanner scanner = new Scanner(System.in);
+        Long length = scanner.nextLong();
+        print("Length is " + length);
+        if (length > 0) {
+            String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            Random random = new Random();
+            try {
+                String fileName = "random-text.txt";
+                FileWriter myWriter = new FileWriter(fileName);
+                for (long i = 0; i < length; i++) {
+                    myWriter.write(String.valueOf(chars.charAt(random.nextInt(chars.length()))));
+                }
+                myWriter.close();
+                print("Successfully wrote to the file " + fileName);
+            } catch (IOException e) {
+                print("An error occurred.");
+                e.printStackTrace();
+            }
 
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter number: ");
-		int number = scanner.nextInt();
-		System.out.println("Number is " + number);
-		if (number > 0) {
-			StringBuilder dataText = new StringBuilder();
-			for (int i = 1; i <= number; i++) {
-				dataText.append(i).append("\n");
-			}
-			try {
-				String fileName = "number-" + number + ".txt";
-				FileWriter myWriter = new FileWriter(fileName);
-				myWriter.write(String.valueOf(dataText));
-				myWriter.close();
-				System.out.println("Successfully wrote to the file " + fileName);
-			} catch (IOException e) {
-				System.out.println("An error occurred.");
-				e.printStackTrace();
-			}
-		} else {
-			System.out.print("the Number must be greater than 0");
-		}
+        } else {
+            print("the Number must be greater than 0");
+        }
+    }
 
-	}
+    private void print(String words) {
+        System.out.println(words);
+    }
 }
